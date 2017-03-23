440532a72a96295b6e1d010f5cf1ba93f2f9ea7e
Sullivan Jot <Sullivan Jot>
2017-03-23 06:04:20 +0100

    Ajout navigation dans les pages

---
+++ site/assets/scripts/menu-selector.js
@@ -0,0 +1,35 @@
+(function($){
+    "use strict";
+    var btnAccueil = document.getElementById("btn-accueil");
+    var btnJeu = document.getElementById("btn-jeu");
+    var btnCommunaute = document.getElementById("btn-communaute");
+    var btnGalerie = document.getElementById("btn-galerie");
+
+    $(btnAccueil).click(function(){
+            $("#div-accueil").show();
+            $("#div-jeu").hide();
+            $("#div-communaute").hide();
+            $("#div-galerie").hide();
+    });
+
+    $(btnJeu).click(function(){
+            $("#div-accueil").hide();
+            $("#div-jeu").show();
+            $("#div-communaute").hide();
+            $("#div-galerie").hide();
+    });
+
+    $(btnCommunaute).click(function(){
+            $("#div-accueil").hide();
+            $("#div-jeu").hide();
+            $("#div-communaute").show();
+            $("#div-galerie").hide();
+    });
+
+    $(btnGalerie).click(function(){
+            $("#div-accueil").hide();
+            $("#div-jeu").hide();
+            $("#div-communaute").hide();
+            $("#div-galerie").show();
+    });
+})(jQuery);
