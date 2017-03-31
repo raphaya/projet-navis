defe143922501fe963ad85ba811e9df65e7cb8da
Raphael <raphaya@laposte.net>
2017-03-31 13:27:19 +0200

    jhg

---
+++ site/assets/scripts/login.js
@@ -0,0 +1,38 @@
+(function ($) {
+    var $login = $('#connect');
+
+
+    $('#connect').on("submit", function (e) {
+
+        var $login = $('#pseudo'),
+            $password = $('#password'),
+            html = '',
+            login = $login.val(),
+            $nameContainer = $('.name');
+        var regexLogin = new RegExp('^[a-zA-Z]{1,}'),
+            regexMail = /^[\w\.-]{3,20}@[\w.]{3,30}\.[a-zA-Z]{2,10}$/;
+            regexPwd = new RegExp('^.{8,}');
+
+        if (regexLogin.test(login)) {
+            $('.erreur-name').remove('.erreur-name');
+        } else {
+            if (!$('.erreur-name')[0]) {
+                $nameContainer.append('<span class="erreur-name"> nom incorrect</span>');
+            }
+            regexTest = true;
+        }
+        
+       irc.ajax({
+            data: {
+                login : $login.val(),
+                password : $password.val(),
+            },
+            url: 'connect.php'
+        });
+
+        e.stopPropagation();
+        e.preventDefault();
+
+    });
+
+})(jQuery);
---
+++ site/assets/scripts/register.js
@@ -0,0 +1,49 @@
+(function ($) {
+    var $register = $('#register');
+
+    $('#register').on("submit", function (e) {
+        var $login = $('#pseudo'),
+            $password = $('#password'),
+            $mail = $('#mail'),
+            html = '';
+                 alert('match');
+            var regexlogin = new RegExp('/^[a-z0-9_-]{3,16}$/'),
+                regexpass = new RegExp('/^[a-z0-9_-]{6,18}$/'),
+                regexemail = new RegExp('/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/');
+
+            var reslogin = regexlogin.test($login.val()),
+                respassword = regexpass.test($password.val()),
+                resmail = regexemail.test($mail.val());
+                
+            console.log($mail.val());
+
+            if(reslogin.test){
+                alert('match');
+            }else{
+                alert('dismatch');
+            }
+            if(respassword.test){
+                alert('match');
+            }else{
+                alert('dismatch');
+            }
+            if(resmail.test){
+                alert('match');
+            }else{
+                alert('dismatch');
+            }
+
+       irc.ajax({
+            data: {
+                login : $pseudo.val(),
+                password : $password.val(),
+                mail : $mail.val(),
+            },
+            url: 'register.php'
+        });
+
+    });
+        e.stopPropagation();
+        e.preventDefault(); 0
+        return false;
+})(jQuery);
