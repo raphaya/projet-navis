eab879ec6c462572de7362bc7f706d814aaf0467
Ludovic <ludovic.machen@gmail.com>
2017-04-21 14:16:25 +0200

    jeu en responsive

--- test-phaser/assets/scripts/boot.js
+++ test-phaser/assets/scripts/boot.js
@@ -8,6 +8,9 @@ var bootState = {
     },
 
     create: function () {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
         game.state.start('load');
     }
 
