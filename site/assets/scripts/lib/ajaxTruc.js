f05ec779f005b57ffc318448f30d8bd7a9f7fb9f
Sullivan <sullivan.jot@gmail.com>
2017-04-26 16:46:36 +0200

    json design

--- site/assets/scripts/lib/ajaxTruc.js
+++ site/assets/scripts/lib/ajaxTruc.js
@@ -1,64 +1,92 @@
-var content = document.getElementById('content'),
-	$content = $(content);
+(function ($) {
+	var content = document.getElementById('content'),
+		$content = $(content);
 
-var patate = function () {
-	$.ajax({
-		method: "get",
-		url: 'views/accueil.html',
-		success: function (html) {
-			var $html = $(html),
-				elem = $html.find(".content-ajax");
-			$(content).html(html).show();
-		}
+	var patate = function () {
+		$.ajax({
+			method: "get",
+			url: 'views/accueil.html',
+			success: function (html) {
+				var $html = $(html),
+					elem = $html.find(".content-ajax");
+				$(content).html(html).show();
+			}
+
+		});
+	};
+
+	$('#btn-accueil').click(patate);
+
+
+
+	$('#btn-galerie').click(function () {
+		$.ajax({
+			method: "get",
+			url: 'views/galerie.html',
+			success: function (html) {
+				var $html = $(html),
+					elem = $html.find(".content-ajax");
+				$(content).html(html).show();
+			}
+		});
+
+		$.ajax({
+			method: 'get',
+			dataType: "json",
+			url: 'assets/content/galerie.json',
+			success: function () { console.log("youpi json"); },
+			error: function (xhr, status) { console.log(status); }
+		});
+
+		$.getJSON('assets/content/galerie.json', function (data) {
+			$.each(data, function (index, d) {
+				$('#contenu-galerie').append('<tr><td class="ship">' + d.img + '</td>' + '<td class="description"><div class="titre">' + d.titre + '</div>' + d.texte + '</td></tr>');
+			});
+		});
 
 	});
-};
-
-$('#btn-accueil').click(patate);
 
 
+	$('#btn-communaute').click(function () {
+		$.ajax({
+			method: "get",
+			url: 'views/communaute.html',
+			success: function (html) {
+				var $html = $(html),
+					elem = $html.find(".content-ajax");
+				$(content).html(html).show();
+			}
+		});
 
-$('#btn-galerie').click(function () {
-	$.ajax({
-		method: "get",
-		url: 'views/galerie.html',
-		success: function (html) {
-			var $html = $(html),
-				elem = $html.find(".content-ajax");
-			$(content).html(html).show();
-		}
+		$.ajax({
+			method: 'get',
+			dataType: "json",
+			url: 'assets/content/communaute.json',
+			success: function () { console.log("youpi json"); },
+			error: function (xhr, status) { console.log(status); }
+		});
+
+		$.getJSON('assets/content/communaute.json', function (data) {
+			$.each(data, function (index, d) {
+				$('#contenu-commu').append('<tr><td class="col-xs-2">' + d.id + '</td><td class="col-xs-8">' + d.name + '</td><td class="col-xs-2">' + d.score + '</td></tr>');
+			});
+		});
 	});
-});
 
-
-$('#btn-communaute').click(function () {
-	$.ajax({
-		method: "get",
-		url: 'views/communaute.html',
-		success: function (html) {
-			var $html = $(html),
-				elem = $html.find(".content-ajax");
-			$(content).html(html).show();
-		}
+	$('#btn-jeu').click(function () {
+		$.ajax({
+			method: "get",
+			url: 'views/jeu.html',
+			success: function (html) {
+				var $html = $(html),
+					elem = $html.find(".content-ajax");
+				$(content).html(html).show();
+			}
+		});
 	});
-});
 
-$('#btn-jeu').click(function () {
-	$.ajax({
-		method: "get",
-		url: 'views/jeu.html',
-		success: function (html) {
-			var $html = $(html),
-				elem = $html.find(".content-ajax");
-			$(content).html(html).show();
-		}
+	$(document).ready(function () {
+		patate();
 	});
-});
 
-$( document ).ready(function() {
-  
-  patate();
-
-
-
-});
+})(jQuery);
